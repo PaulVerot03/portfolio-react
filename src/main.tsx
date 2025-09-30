f4b5c4287f1e3240ce91a3a0f7ff4585509e60ed
paulverot <pauljeanlouisverot@protonmail.com>
2025-09-30 22:57:28 +0200

    changed basename to fit cloudflare

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './style/styles.css'           // ← Import global CSS
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
